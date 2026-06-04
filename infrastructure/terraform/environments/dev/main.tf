ca67077daf42b9c1da6920b853962f8e014bad2d
Du4rte1 <duarte.guide.almeida@gmail.com>
2026-06-04 21:15:11 +0100

    Add RDS and SQS infrastructure

--- infrastructure/terraform/environments/dev/main.tf
+++ infrastructure/terraform/environments/dev/main.tf
@@ -1,16 +1,33 @@
 module "vpc" {
   source = "../../modules/vpc"
 
-  vpc_cidr            = "10.0.0.0/16"
-  public_subnet_cidr  = "10.0.1.0/24"
-  private_subnet_cidr = "10.0.2.0/24"
+  vpc_cidr = "10.0.0.0/16"
+
+  public_subnet_a_cidr = "10.0.1.0/24"
+  public_subnet_b_cidr = "10.0.2.0/24"
+
+  private_subnet_a_cidr = "10.0.3.0/24"
+  private_subnet_b_cidr = "10.0.4.0/24"
 }
 
 module "ec2" {
   source = "../../modules/ec2"
 
   vpc_id    = module.vpc.vpc_id
-  subnet_id = module.vpc.public_subnet_id
+  subnet_id = module.vpc.public_subnet_a_id
 
   key_name = "cloud-project-key"
+}
+
+module "rds" {
+  source = "../../modules/rds"
+
+  vpc_id = module.vpc.vpc_id
+
+  private_subnet_a_id = module.vpc.private_subnet_a_id
+  private_subnet_b_id = module.vpc.private_subnet_b_id
+}
+
+module "sqs" {
+  source = "../../modules/sqs"
 }
--- infrastructure/terraform/environments/dev/outputs.tf
+++ infrastructure/terraform/environments/dev/outputs.tf
@@ -1,3 +1,11 @@
 output "ec2_public_ip" {
   value = module.ec2.public_ip
 }
+
+output "queue_url" {
+  value = module.sqs.queue_url
+}
+
+output "queue_arn" {
+  value = module.sqs.queue_arn
+}
